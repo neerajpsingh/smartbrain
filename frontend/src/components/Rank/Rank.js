56a313a6873884e9a45fe7cb92a6be1396909e28
Neeraj Pratap Singh <sp102712hal@gmail.com>
2019-10-15 20:47:43 +0530

    Add files via upload

---
+++ frontend/src/components/Rank/Rank.js
@@ -0,0 +1,22 @@
+import React from'react';
+
+
+const Rank = () => {
+	return(
+		<div>
+			<div className='center white f3'>
+				{'Neeraj,your current rank is...'}
+			</div>
+			<div className='center white f1'>
+				{'#2'}
+			</div>
+		</div>
+
+
+
+
+		);
+
+}
+
+export default Rank;
